b980ca28eb3cae876c1a86d25d349614d08c5572
igorfreire.3 <igorfreire110@gmail.com>
2023-07-18 17:04:05 -0300

    arrumando detalhes no carrinho

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,7 +1,27 @@
 import { useState } from "react";
 import { getItem, setItem } from "../services/localStorageFuncs";
 import { BsFillCartDashFill } from "react-icons/bs";
+import { FaBarcode } from "react-icons/fa";
+import { BsFillCreditCard2FrontFill } from "react-icons/bs";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
+import InputMask from "react-input-mask";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 import "../styles/cart.css";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "white",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Cart = () => {
   const [data, setData] = useState(getItem("carrinhoYt") || []);
@@ -10,12 +30,73 @@ const Cart = () => {
   const [cidade, setCidade] = useState("");
   const [rua, setRua] = useState("");
   const [custo, setCusto] = useState(0);
-  const subTotal = data.reduce((acc, cur) => acc + cur.price, 0);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [paymentMethod, setPaymentMethod] = useState("");
+  const [creditCard, setCreditCard] = useState("");
+  const [cardholderName, setCardholderName] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [securityCode, setSecurityCode] = useState("");
+  const [quantidades, setQuantidades] = useState({});
+  const subTotal = data.reduce(
+    (acc, cur) => acc + cur.price * (quantidades[cur.id] || cur.quantity || 0),
+    0
+  );
+  const isCreditCardSelected = paymentMethod === "creditCard";
+  const isBoletoSelected = paymentMethod === "boleto";
 
   const removeItem = (id) => {
     const arrFilter = data.filter((item) => item.id !== id);
     setData(arrFilter);
     setItem("carrinhoYt", arrFilter);
+  };
+
+  const handlePaymentMethodChange = (method) => {
+    setPaymentMethod(method);
+  };
+
+  const incrementarQuantidade = (id) => {
+    setQuantidades((prevQuantidades) => ({
+      ...prevQuantidades,
+      [id]: (prevQuantidades[id] || 0) + 1,
+    }));
+  };
+
+  const decrementarQuantidade = (id) => {
+    setQuantidades((prevQuantidades) => ({
+      ...prevQuantidades,
+      [id]: (prevQuantidades[id] || 0) - 1,
+    }));
+  };
+
+  const handlePaymentSubmit = (e) => {
+    e.preventDefault();
+
+    if (paymentMethod === "boleto") {
+      // Gerar boleto PDF
+
+      // Exemplo de toast de boleto gerado com sucesso
+      toast.success("Boleto gerado com sucesso!");
+
+      // Limpar campos
+      setCardholderName("");
+      setCreditCard("");
+    } else if (paymentMethod === "creditCard") {
+      // Lógica para processar pagamento com cartão de crédito
+
+      // Exemplo de toast de pagamento confirmado com sucesso
+      toast.success("Pagamento confirmado!");
+
+      // Limpar campos
+      setCreditCard("");
+      setCardholderName("");
+      setExpirationDate("");
+      setSecurityCode("");
+    }
+
+    handleClose();
+    setPaymentMethod("");
   };
 
   const checkCep = () => {
@@ -41,20 +122,35 @@ const Cart = () => {
               <h4 className="title">{item.title}</h4>
               <img src={item.thumbnail} alt="Product" />
               <div className="price-remove">
-                <h4
+                <h4 className="price">R$ {item.price}</h4>
+                <span
                   style={{
-                    fontFamily: "Arial",
-                    fontSize: "18px",
-                    color: "#ff0000",
-                    fontWeight: "bold",
                     display: "flex",
+                    justifyContent: "center",
                     alignItems: "center",
-                    textShadow: "1px 1px 2px rgba(0, 0, 0, 0.3)",
-                    marginLeft: "6.8vh",
                   }}
                 >
-                  R$ {item.price}
-                </h4>
+                  Quant.
+                </span>
+                <div className="quantidade">
+                  <SlArrowLeft
+                    className={
+                      (quantidades[item.id] || item.quantity) <= 1
+                        ? "disabled"
+                        : "seta"
+                    }
+                    onClick={() => {
+                      if ((quantidades[item.id] || item.quantity) > 0) {
+                        decrementarQuantidade(item.id);
+                      }
+                    }}
+                  />
+                  <h3>{quantidades[item.id] || item.quantity || 0}</h3>
+                  <SlArrowRight
+                    onClick={() => incrementarQuantidade(item.id)}
+                    className="seta"
+                  />
+                </div>
                 <button onClick={() => removeItem(item.id)}>
                   <BsFillCartDashFill />
                   <span className="remove-text">Remove To Cart</span>
@@ -63,7 +159,7 @@ const Cart = () => {
             </div>
           ))
         ) : (
-          <p>No items in the cart</p>
+          <p className="vazio">O seu carrinho está vazio.</p>
         )}
       </div>
       <div className="cart-summary">
@@ -99,17 +195,146 @@ const Cart = () => {
           <span>Entrega:</span>
           <span>R$ {custo}</span>
         </div>
-        <hr
-          style={{
-            marginTop: "2vh",
-            marginBottom: "2vh",
-          }}
-        />
+        <hr className="divider" />
         <div className="summary-item">
-          <span style={{ fontWeight: "bold" }}>Total</span>
-          <span style={{ fontWeight: "bold" }}>R$ {subTotal + custo}</span>
+          <span className="total-label">Total:</span>
+          <span className="total-price">R$ {subTotal + custo}</span>
         </div>
-        <button className="checkout-button">Finalizar Compra</button>
+
+        <button className="checkout-button" onClick={handleOpen}>
+          Finalizar Compra
+        </button>
+
+        <Modal open={open} onClose={handleClose}>
+          <Box sx={style}>
+            <h2 className="title-modal">Escolha a forma de pagamento</h2>
+            <div className="payment-methods">
+              <div
+                className={`payment-method ${
+                  paymentMethod === "creditCard" ? "active" : ""
+                }`}
+              >
+                <input
+                  type="radio"
+                  name="paymentMethod"
+                  id="creditCard"
+                  value="creditCard"
+                  checked={paymentMethod === "creditCard"}
+                  onChange={() => handlePaymentMethodChange("creditCard")}
+                />
+
+                <label htmlFor="creditCard" className="credito">
+                  <span>Cartão de Crédito</span>
+                  <BsFillCreditCard2FrontFill className="payment-icon" />
+                </label>
+              </div>
+              <div
+                className={`payment-method ${
+                  paymentMethod === "boleto" ? "active" : ""
+                }`}
+              >
+                <input
+                  type="radio"
+                  name="paymentMethod"
+                  id="boleto"
+                  value="boleto"
+                  checked={paymentMethod === "boleto"}
+                  onChange={() => handlePaymentMethodChange("boleto")}
+                />
+                <label htmlFor="boleto" className="boleto">
+                  <span>Boleto Bancário</span>
+                  <FaBarcode className="payment-icon" />
+                </label>
+              </div>
+            </div>
+
+            {paymentMethod === "boleto" && (
+              <div
+                className={`boleto-form ${isBoletoSelected ? "show" : "hide"}`}
+              >
+                <h3>Boleto Bancário</h3>
+                <p>Preencha seus dados para gerar o boleto:</p>
+                <input
+                  type="text"
+                  placeholder="Nome"
+                  value={cardholderName}
+                  onChange={(e) => setCardholderName(e.target.value)}
+                  required
+                />
+                <InputMask
+                  type="text"
+                  mask="999.999.999-99"
+                  placeholder="CPF"
+                  value={creditCard}
+                  onChange={(e) => setCreditCard(e.target.value)}
+                  required
+                />
+                <button
+                  className="payment-button"
+                  onClick={handlePaymentSubmit}
+                >
+                  Gerar Boleto
+                </button>
+              </div>
+            )}
+            {paymentMethod === "creditCard" && (
+              <form
+                onSubmit={handlePaymentSubmit}
+                className={`credit-card-inputs ${
+                  isCreditCardSelected ? "show" : "hide"
+                }`}
+              >
+                <h3>Informações do Cartão de Crédito</h3>
+                <div className="credit-card-inputs">
+                  <label htmlFor="creditCardNumber">Número do Cartão:</label>
+                  <input
+                    type="text"
+                    id="creditCardNumber"
+                    value={creditCard}
+                    onChange={(e) => setCreditCard(e.target.value)}
+                    required
+                  />
+
+                  <label htmlFor="cardholderName">Nome do Titular:</label>
+                  <input
+                    type="text"
+                    id="cardholderName"
+                    value={cardholderName}
+                    onChange={(e) => setCardholderName(e.target.value)}
+                    required
+                  />
+
+                  <label htmlFor="expirationDate">Data de Expiração:</label>
+                  <InputMask
+                    mask="99/99"
+                    type="text"
+                    id="expirationDate"
+                    value={expirationDate}
+                    onChange={(e) => setExpirationDate(e.target.value)}
+                    required
+                  />
+
+                  <label htmlFor="securityCode">Código de Segurança:</label>
+                  <InputMask
+                    mask="999"
+                    type="text"
+                    id="securityCode"
+                    value={securityCode}
+                    onChange={(e) => setSecurityCode(e.target.value)}
+                    required
+                  />
+                </div>
+
+                <button type="submit" className="checkout-button">
+                  Confirmar Pagamento
+                </button>
+              </form>
+            )}
+          </Box>
+        </Modal>
+
+        {/* Toast Container */}
+        <ToastContainer />
       </div>
     </div>
   );
--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -6,10 +6,25 @@ import "../styles/store.css";
 export const Store = () => {
   const [data, setData] = useState([]);
   const [cart, setCart] = useState(getItem("carrinhoYt") || []);
+  const [searchValue, setSearchValue] = useState("");
+
+  const fetchApi = async (searchTerm, setData) => {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?category=MLB1051&q=${encodeURIComponent(
+      searchTerm
+    )}`;
+    const response = await fetch(url);
+    const objJson = await response.json();
+    setData(objJson.results);
+  };
+
+  useEffect(() => {
+    fetchApi(searchValue, setData);
+  }, [searchValue]);
 
   useEffect(() => {
     const fetchApi = async () => {
-      const url = "https://api.mercadolibre.com/sites/MLB/search?q=celular";
+      const url =
+        "https://api.mercadolibre.com/sites/MLB/search?category=MLB1051&";
       const response = await fetch(url);
       const objJson = await response.json();
       setData(objJson.results);
@@ -18,14 +33,15 @@ export const Store = () => {
   }, []);
 
   const handleClick = (obj) => {
-    const element = cart.find((e) => e.id == obj.id);
+    const element = cart.find((e) => e.id === obj.id);
     if (element) {
-      const arrFilter = cart.filter((e) => e.id != obj.id);
+      const arrFilter = cart.filter((e) => e.id !== obj.id);
       setCart(arrFilter);
       setItem("carrinhoYt", arrFilter);
     } else {
-      setCart([...cart, obj]);
-      setItem("carrinhoYt", [...cart, obj]);
+      const updatedCart = [...cart, { ...obj, quantity: 1 }];
+      setCart(updatedCart);
+      setItem("carrinhoYt", updatedCart);
     }
   };
 
@@ -40,6 +56,21 @@ export const Store = () => {
       >
         Store
       </h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+        />
+        <button
+          className="search-button"
+          onClick={() => fetchApi(searchValue, setData)}
+        >
+          Search
+        </button>
+      </div>
       <div className="store-container">
         {data.map((e) => (
           <div className="store-item" key={e.id}>
@@ -49,7 +80,7 @@ export const Store = () => {
               style={{
                 fontFamily: "Arial",
                 fontSize: "18px",
-                color: "#ff0000", // ou qualquer outra cor que desejar
+                color: "#ff0000",
                 fontWeight: "bold",
                 display: "flex",
                 alignItems: "center",
@@ -58,29 +89,23 @@ export const Store = () => {
             >
               <span style={{ marginRight: "5px" }}>R$</span> {e.price}
             </h4>
-            <button
-              onClick={() => handleClick(e)}
-              className={`add-to-cart-btn ${
-                cart.some((itemCart) => itemCart.id === e.id)
-                  ? "added"
-                  : "store-item"
-              }`}
-            >
-              {cart.some((itemCart) => itemCart.id == e.id) ? (
-                <button className="add-to-cart-btn added">
-                  <BsFillCartCheckFill />
-                  <span>Added to Cart</span>
-                </button>
-              ) : (
-                <button
-                  className="add-to-cart-btn"
-                  onClick={() => handleClick(e)}
-                >
-                  <BsFillCartPlusFill />
-                  <span>Add to Cart</span>
-                </button>
-              )}
-            </button>
+            {cart.some((itemCart) => itemCart.id === e.id) ? (
+              <button
+                className="add-to-cart-btn added"
+                onClick={() => handleClick(e)}
+              >
+                <BsFillCartCheckFill />
+                <span>Added to Cart</span>
+              </button>
+            ) : (
+              <button
+                className="add-to-cart-btn"
+                onClick={() => handleClick(e)}
+              >
+                <BsFillCartPlusFill />
+                <span>Add to Cart</span>
+              </button>
+            )}
           </div>
         ))}
       </div>
